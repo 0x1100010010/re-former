ea5aa7b9708edbd045210600503b9291d90068f5
bashforger <bash.forger@gmail.com>
2021-01-06 19:53:55 +0500

    Rubocop debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-  
+
     if @user.save
       redirect_to new_users_path
     else
       render :new
     end
-
   end
 
   def edit
@@ -36,7 +35,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username,:email,:password)
+    params.require(:user).permit(:username, :email, :password)
   end
-
 end
