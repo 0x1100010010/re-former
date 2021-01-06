f1c5a849a3623e290a0eb393b078bf2cba4d04ea
bashforger <bash.forger@gmail.com>
2021-01-06 19:33:14 +0500

    Finished Update feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,12 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
   private
 
   def user_params
