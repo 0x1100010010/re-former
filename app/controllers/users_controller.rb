77e1df9dc023bdb2a26a50f50bc65a03e93a8ad1
bashforger <bash.forger@gmail.com>
2021-01-07 14:57:23 +0500

    inter debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     puts params[:id]
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
